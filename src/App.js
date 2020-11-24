d603d99e0d5385e56f7139e26ff2d844172c18fa
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2020-11-24 16:11:15 +0900

    11月28日勉強会用の例を修正

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import UseStateArray from './components/UseStateArray'
+import Title from './components/Title'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <Title/>
+        <UseStateArray/>
+      </div>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const data = [
+  { id: 1, name: "Pitang" },
+  { id: 2, name: "Son" },
+  { id: 3, name: "Kamikin" },
+  { id: 4, name: "Maki" },
+  { id: 5, name: "yoko" }
+];
